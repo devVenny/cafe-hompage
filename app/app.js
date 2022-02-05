84150d193e4dbb64f6b97a93d10b65e628381689
vennylee <hyungsup42134@gmail.com>
2022-02-05 17:33:55 +0900

    login 프론트단 제어

--- app/app.js
+++ app/app.js
@@ -3,13 +3,14 @@
 // 모듈
 const express = require("express");
 const app = express();
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 세팅
-app.set("views", "views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 // 미들 웨어
 app.use("/", home);
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
---
+++ app/src/public/js/login.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const id = document.getElementById("id"),
+  psword = document.getElementById("psword"),
+  button = document.getElementById("button");
+
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.error("데이터 패치 실패"));
+}
