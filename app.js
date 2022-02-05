131160ef5eaa0b90679320d42274f7a8b572a6e0
vennylee <hyungsup42134@gmail.com>
2022-02-05 13:50:08 +0900

    onBoard 화면 view모델로 분리

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.get("/login", (req, res) => {
+  res.render("onBoard/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("onBoard/register");
+});
+
+// app.get("/ ", (req, res) => {
+//   res.send("dㅣ곳ㅇ");
+// });
+
 app.listen(3000, () => {
   console.log("서버 가동");
 });
